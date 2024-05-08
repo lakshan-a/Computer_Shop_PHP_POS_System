55f4894d37cde8cd94c0776711acc24948db610b
lakshan <lakshanrashmika16302@gmail.com>
2024-05-08 21:38:39 +0530

    updated

--- admin/code.php
+++ admin/code.php
@@ -95,4 +95,28 @@ if(isset($_POST['updateAdmin'])){
     }
 }
 
+
+
+if(isset($_POST['saveCategory'])){
+
+    $name = validate($_POST['name']);
+    $description = validate($_POST['description']);
+    $status = isset($_POST['status']) == true ? 1:0;
+
+    $data = [
+        'name' => $name,
+        'description' => $description,
+        'status' => $status
+    ];
+    $result = insert('categories',$data);
+
+
+    if($result){
+        redirect('categories.php','Category Created Successfully!');
+    }else{
+        redirect('categories-create.php','Something Went Wrong!');
+    }
+
+}
+
 ?>
