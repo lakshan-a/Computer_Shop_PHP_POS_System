96cd7c3becc6f9b2a765caebb0330280c6686085
lakshan <lakshanrashmika16302@gmail.com>
2024-05-10 12:06:23 +0530

    updated

--- admin/categories-create.php
+++ admin/categories-create.php
@@ -4,7 +4,7 @@
     <div class="card mt-4 shadow-sm">
         <div class="card-header">
             <h4 class="mb-0">Add Category
-                <a href="categories.php" class="btn btn-primary float-end">Back</a>
+                <a href="categories.php" class="btn btn-danger float-end">Back</a>
             </h4>
         </div>
         <div class="card-body">
