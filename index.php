d79aedf73db08638cc93bdf8a1d6a963e27c7a90
lakshan <lakshanrashmika16302@gmail.com>
2024-05-17 22:51:28 +0530

    finished project

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php include('includes/header.php'); ?>
 
 
-    <div class="py-5">
-        <div class="container mt-5">
+    <div class="py-5 mainPosBg">
+        <div class="container my-5">
             <div class="row">
-                <div class="col-md-12">
-                    <h1>Pos System</h1>
+                <div class="col-md-12 py-5 text-center">
 
+                    <?php alertMessage(); ?>
+
+                    <h1 class="mt-3">Pos System</h1>
+
+                    <?php if(!isset($_SESSION['loggedIn'])) : ?>
                     <a href="login.php" class="btn btn-primary mt-4">Login</a>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
